c9f25f40adbafe40ab39c6f4ee4978c4f40d9d10
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-07-13 02:24:32 +0300

    index.js - check winner

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let game_squares = document.getElementsByClassName("btn");
 
 
 let counter = 1;
-
+let check_Status = 0;
 for (let i = 0; i <= 8; i++) {
   game_squares[i].addEventListener("click", function () {
     counter += 1;
@@ -12,51 +12,70 @@ for (let i = 0; i <= 8; i++) {
     } else {
       game_squares[i].innerHTML = "O";
     }
-
-    if (counter === 10) {
-      check_winner();
-    }
+    check_Status += 1;
+    check_winner();
   });
 }
 
 function check_winner() {
-  for (let i = 0; i <= 7; i++) {
-    let counter = 1;
-    if (i !== 2) {
+  console.log("check");
+  for (let i = 0; i <= 8; i++) {
+    let checker = 1;
+    if (i <= 2) {
       for (let j = 1; j <= 3; j++) {
-        let square_row = document.querySelector(`.col-${j}.row-${counter}`);
+        let square_row = document.querySelector(`.col-${j}.row-${checker}`);
         if (square_row.innerHTML !== "") {
-          winning_counter(square_row);
+          winning_counter(square_row.textContent);
         }
       }
-      counter += 1;
-    } else if (i <= 5) {
-      counter -= 3;
+      winning_counter("NO WiNNER");
+      checker += 1;
+    } else if (i <= 5 && i > 2) {
+      checker = 1;
       for (let j = 1; j <= 3; j++) {
-        let square_row = document.querySelector(`.col-${counter}.row-${j}`);
-        if (square_row.innerHTML !== "") {
-          winning_counter(square_row);
+        let square_row = document.querySelector(`.col-${checker}.row-${j}`);
+        if (square_row.textContent !== "") {
+          winning_counter(square_row.textContent);
         }
       }
-      counter += 1;
-    } else {
+      winning_counter("NO WiNNER");
+      checker += 1;
+    } else if (i === 6) {
       for (let j = 1; j <= 3; j++) {
         let square_row = document.querySelector(`.col-${j}.row-${j}`);
         if (square_row.innerHTML !== "") {
-          winning_counter(square_row);
+          winning_counter(square_row.textContent);
         }
       }
+      winning_counter("NO WiNNER");
+    } else {
+      checker = 3;
+      for (let j = 1; j <= 3; j++) {
+        let square_row = document.querySelector(`.col-${checker}.row-${j}`);
+        if (square_row.innerHTML !== "") {
+          winning_counter(square_row.textContent);
+        }
+        checker -= 1;
+      }
+      winning_counter("NO WiNNER");
     }
   }
 }
 
+let winning_counter_X = 0;
+let winning_counter_O = 0;
 function winning_counter(square) {
-  //   let winning_counter_X = 0;
-  //   let winning_counter_Y = 0;
-  //   if (square.innerHTML === "X") {
-  //     winning_counter_X += 1;
-  //   } else {
-  //     winning_counter_Y += 1;
-  //   }
-  console.log(square.innerHTML);
+  if (square === "X") {
+    winning_counter_X += 1;
+  } else if (square === "O") {
+    winning_counter_O += 1;
+  } else {
+    winning_counter_X = 0;
+    winning_counter_O = 0;
+  }
+  if (winning_counter_X === 3) {
+    console.log("Xxxxx");
+  } else if (winning_counter_O === 3) {
+    console.log("OOOOOOOOO");
+  }
 }

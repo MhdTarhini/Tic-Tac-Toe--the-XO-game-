38ca5fb4b86dfe1109c3368a658f6e18041521fd
Mohamad Tarhini <mohamed_tarhini@live.com>
2023-07-13 01:26:52 +0300

    index.js - fix the eventlistener

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 let game_card = document.querySelector(".game-card");
 let game_squares = document.getElementsByClassName("btn");
 
+
+let counter = 1;
+
 for (let i = 0; i <= 8; i++) {
   game_squares[i].addEventListener("click", function () {
-    game_squares[i].classList.add("blue");
+    counter += 1;
+    if (counter % 2 === 0) {
+      game_squares[i].innerHTML = "X";
+    } else {
+      game_squares[i].innerHTML = "O";
+    }
+
+    if (counter === 10) {
+      check_winner();
+    }
   });
 }
-game_card.addEventListener("click", function () {
-  for (let i = 0; i <= 8; i++) {
-    if (game_squares[i].classList.contains("blue")) {
-      game_squares[i].classList.add("red");
+
+function check_winner() {
+  for (let i = 0; i <= 7; i++) {
+    let counter = 1;
+    if (i !== 2) {
+      for (let j = 1; j <= 3; j++) {
+        let square_row = document.querySelector(`.col-${j}.row-${counter}`);
+        if (square_row.innerHTML !== "") {
+          winning_counter(square_row);
+        }
+      }
+      counter += 1;
+    } else if (i <= 5) {
+      counter -= 3;
+      for (let j = 1; j <= 3; j++) {
+        let square_row = document.querySelector(`.col-${counter}.row-${j}`);
+        if (square_row.innerHTML !== "") {
+          winning_counter(square_row);
+        }
+      }
+      counter += 1;
+    } else {
+      for (let j = 1; j <= 3; j++) {
+        let square_row = document.querySelector(`.col-${j}.row-${j}`);
+        if (square_row.innerHTML !== "") {
+          winning_counter(square_row);
+        }
+      }
     }
   }
-});
+}
+
+function winning_counter(square) {
+  //   let winning_counter_X = 0;
+  //   let winning_counter_Y = 0;
+  //   if (square.innerHTML === "X") {
+  //     winning_counter_X += 1;
+  //   } else {
+  //     winning_counter_Y += 1;
+  //   }
+  console.log(square.innerHTML);
+}
